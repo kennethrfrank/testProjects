5a1e0b1478287d169e1f370e3cedeaa47de04c8f
kennethrfrank <kennethrfrankjr@gmail.com>
2017-10-31 10:31:09 -0500

    Add files via upload

--- BigMap/script.js
+++ BigMap/script.js
@@ -23,33 +23,45 @@
 
 
 
+	
 function initMap(){
-		  	var map = new google.maps.Map($('#map'), {
-		          zoom: 12,
-		          center: google.maps.LatLng(33, -96)
+	var uluru = {lat: 40.137, lng : -97.85}
+		  	var map = new google.maps.Map(document.getElementById('map'), {
+		          zoom: 4,
+		          center: uluru
 		        });
+
 		var results = [];
-		var lats = [];
-		var longs = [];
+		var markers = [];
+		var x;
+				var y;
 		//var sa = "address="+streetAdd.trim().replace(" ", "+");
-		var zips = [75034, 93300, 37235];
+		var zips = [75034, 93300, 37235, 92200, 38321, 57654];
 
 		for(var i = 0 ; i<zips.length; i++){
 			var zipC = "zip="+zips[i];
 			
-		
+				
 			fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+zipC+"&key=AIzaSyAVEhxwJx2QmvpQsTvOT0jbpGlwA2cAS6o").then((response) => response.json()).then(function(response){
 				results.push(response.results);
 				
-				// lats[i] = response.results[0].geometry.location.lat;
-				// longs[i] = response.results[0].geometry.location.lng;
-				// 	var uluru = {lat: lats[i], lng : longs[i]}
-			var marker = new google.maps.Marker({
-		          			position: google.maps.LatLng(lats[i], longs[i]),
-			          		map: map
+				x = response.results[0].geometry.location.lat;
+				y = response.results[0].geometry.location.lng;
+				
+			var Muluru = {lat: x, lng : y}
+					var marker = new google.maps.Marker({
+		          			position: Muluru,
+		          			map: map,
+			          		label: "map"
 			});
-					
+					markers.push(marker);
+					var bool = (i >= (zips.length - 1)) && (markers.length>=6);
 
+					if(bool){
+						console.log("cluster triggered");
+						cluster(map, markers);
+
+					}
 
 			}, function(){
 				console.log("Could not consolidate data at this time.");
@@ -58,4 +70,14 @@ function initMap(){
 		
 			
 		}
+
+      	}
+
+function cluster(map, markers){
+	var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+	return markerCluster;
 }
+
+
+$(document).ready();
